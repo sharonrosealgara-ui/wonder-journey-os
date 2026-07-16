dc78422e5634fd2d0821f20a2f07b72192490a8a
Sharon Rose Algara <sharonrosealgara@gmail.com>
2026-07-16 16:49:07 +0800

    health rev3: force clean build to pick up current dashboard values

--- functions/api/health.ts
+++ functions/api/health.ts
@@ -7,7 +7,7 @@ import { json, type Ctx } from "../_shared";
 export const onRequest = async ({ env }: Ctx): Promise<Response> => {
   return json({
     ok: true,
-    builtAt: "2026-07-16",
+    builtAt: "2026-07-16-rev3",
     config: {
       livekitUrl: !!env.LIVEKIT_URL,
       livekitKey: !!env.LIVEKIT_API_KEY,
